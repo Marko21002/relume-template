94013e080b6ef450ee36a4cc367dad45ee8eb31f
Marko21002 <markgufman21@gmail.com>
2025-04-10 05:43:57 +0200

    Remove unused Layout1 component from main page for cleaner code structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Navbar2 } from "@/components/Navbar2";
 import { Header1 } from "@/components/Header1";
 import { Pricing18 } from "@/components/Pricing5";
-import { Layout1 } from "@/components/Layout1";
 import { Contact15 } from "@/components/Contact15";
 import { Footer1 } from "@/components/Footer1";
 import { Testimonial17 } from "@/components/Testimonial";
@@ -17,7 +16,7 @@ export default function Home() {
       <Pricing18 />
       <Cta25 />
       <Faq1 />
-      {/* <Layout1 /> */}
+
       <Contact15 />
       <Footer1 />
     </div>
